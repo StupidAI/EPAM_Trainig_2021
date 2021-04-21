e1aa4c326efe462ea81d7ae112496fc5921d4a67
minenkoea <minenkoea@gmail.com>
2021-04-21 23:29:47 +0500

    Homework1 Task03, 04. Reformat tests with parametrize

--- homework1/task03/tests/test_task03.py
+++ homework1/task03/tests/test_task03.py
@@ -1,17 +1,17 @@
 import os
 
+import pytest
 from task03.hw.task03 import find_maximum_and_minimum
 
 os.chdir(r"./homework1/task03/tests")
 
 
-def test_positive_for_sorted_dataset():
-    """Testing function on sample example"""
-    result = find_maximum_and_minimum("task03_dataset1.txt")
-    assert result == (1, 5)
-
-
-def test_positive_for_zero_dataset():
-    """Testing function on dataset with zero values"""
-    result = find_maximum_and_minimum("task03_dataset2.txt")
-    assert result == (0, 0)
+@pytest.mark.parametrize(
+    "data, result",
+    [
+        ("task03_dataset1.txt", (1, 5)),
+        ("task03_dataset2.txt", (0, 0)),
+    ],
+)
+def test_positive_for_sorted_dataset(data, result):
+    assert find_maximum_and_minimum(data) == result
--- homework1/task04/tests/test_task04.py
+++ homework1/task04/tests/test_task04.py
@@ -1,35 +1,13 @@
+import pytest
 from task04.hw.task04 import check_sum_of_four
 
 
-def test_zero():
-    """
-    Testing function with zero result:
-    a = [1]
-    b = [0]
-    c = [0]
-    d = [1]
-    """
-    a = [1]
-    b = [0]
-    c = [0]
-    d = [1]
-    result = check_sum_of_four(a, b, c, d)
-
-    assert result == 0
-
-
-def test_one():
-    """
-    Testing function on simple dataset:
-    a = [1]
-    b = [0]
-    c = [0]
-    d = [-1]
-    """
-    a = [1]
-    b = [0]
-    c = [0]
-    d = [-1]
-    result = check_sum_of_four(a, b, c, d)
-
-    assert result == 1
+@pytest.mark.parametrize(
+    "a, b, c, d, result",
+    [
+        ([1], [0], [0], [1], 0),
+        ([1], [0], [0], [-1], 1),
+    ],
+)
+def test_zero(a, b, c, d, result):
+    assert check_sum_of_four(a, b, c, d) == result
