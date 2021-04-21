fea214af0dd3b3be2d40147c7722f5d52441de1a
minenkoea <minenkoea@gmail.com>
2021-04-21 23:25:29 +0500

    Homework1 sample_project. Bugfixes. Add parametrize tests

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -1,17 +1,9 @@
 import pytest
-from calculator.calc import check_power_of_2
+from sample_project.calculator.calc import check_power_of_2
 
 
-def test_positive_case():
-    """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(65536)
-
-
-def test_negative_case():
-    """Testing that non-powers of 2 give False"""
-    assert not check_power_of_2(12)
-
-
-def test_positive_case_mycase():
-    """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(8)
+@pytest.mark.parametrize(
+    "data, result", [(65536, True), (12, False), (0, False), (-8, False)]
+)
+def test_calc(data, result):
+    assert check_power_of_2(data) == result
