410e423bb740b807af27374f7cef879f1405b0b1
minenkoea <minenkoea@gmail.com>
2021-04-21 23:27:31 +0500

    Homework1 Task02. Reformat code

--- homework1/task02/hw/task02.py
+++ homework1/task02/hw/task02.py
@@ -15,22 +15,13 @@ def check_fibonacci(data: Sequence[int]) -> bool:
     result = False
     if len(data) < 3:
         return False
-    else:
-        if data[0] == n and data[1] == n_next:
-            for d in data[2:]:
-                n, n_next = n_next, n + n_next
-                if d != n_next:
-                    return False
-                else:
-                    result = True
+    if data[0] != n and data[1] != n_next:
+        while data[0] > n:
+            n, n_next = n_next, n + n_next
+    for d in data:
+        if d != n:
+            return False
         else:
-            while data[0] > n_next:
-                n, n_next = n_next, n + n_next
-            if data[0] == n_next:
-                for d in data[1:]:
-                    n, n_next = n_next, n + n_next
-                    if d != n_next:
-                        return False
-                    else:
-                        result = True
+            result = True
+        n, n_next = n_next, n + n_next
     return result
--- homework1/task02/tests/test_task02.py
+++ homework1/task02/tests/test_task02.py
@@ -1,50 +1,15 @@
+import pytest
 from task02.hw.task02 import check_fibonacci
 
 
-def test_positive_from_zero_seq():
-    """Testing that sequence is Fibonacci: from 0, 1, 1 value"""
-    data_to_process = [
-        0,
-        1,
-        1,
-        2,
-        3,
-        5,
-        8,
-        13,
-        21,
-        34,
-        55,
-        89,
-        144,
-        233,
-        377,
-        610,
-        987,
-        1597,
-        2584,
-        4181,
-        6765,
-    ]
-    assert check_fibonacci(data_to_process)
-
-
-def test_positive_from_random_seq():
-    """Testing that sequence is Fibonacci: from random value"""
-    data_to_process = [89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
-    assert check_fibonacci(data_to_process)
-
-
-def test_negative_from_random_seq():
-    """Testing that sequence is not Fibonacci: from random non Fibonacci value"""
-    data_to_process = [4, 6, 10, 16]
-    # data_to_process = [4181, 6765]
-    # data_to_process = [6765]
-    assert not check_fibonacci(data_to_process)
-
-
-def test_negative_from_seq_of_two():
-    """Testing that sequence is incorrect: len less than 3"""
-    data_to_process = [4181, 6765]
-    # data_to_process = [6765]
-    assert not check_fibonacci(data_to_process)
+@pytest.mark.parametrize(
+    "data_to_process, result",
+    [
+        ([0, 1, 1, 2, 3, 5, 8], True),
+        ([89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765], True),
+        ([4, 6, 10, 16], False),
+        ([4181, 6765], False),
+    ],
+)
+def test_fonacci_sequense(data_to_process, result):
+    assert check_fibonacci(data_to_process) == result
